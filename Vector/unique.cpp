69951841e483e2701d5df2c784c28af6c60ed35a
Mahmud Hasan <mahmud474849@gmail.com>
2024-01-30 22:01:57 +0600

    unique

---
+++ Vector/unique.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int s;
+    cout << "Enter the size: ";
+    cin >> s;
+
+    vector<int> v;
+
+    while (s--)
+    {
+        int e;
+        cin >> e;
+
+        v.push_back(e);
+    }
+
+    sort(v.begin(), v.end());
+    v.erase(unique(v.begin(), v.end()), v.end());
+
+    for (int element : v)
+    {
+        cout << element << " ";
+    }
+
+    return 0;
+}
